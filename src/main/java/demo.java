e4344577be6d7d607c06008413017f961ca8764f
chenminglin <507545713@qq.com>
2021-05-17 19:56:48 +0800

    first commit

---
+++ src/main/java/demo.java
@@ -0,0 +1,7 @@
+public class demo {
+    public static void main(String[] args) {
+        System.out.println((int)'我');
+        System.out.println((int)'你');
+        System.out.println((int)'他');
+    }
+}
